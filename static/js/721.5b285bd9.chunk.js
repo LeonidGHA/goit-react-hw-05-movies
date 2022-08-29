61f3df73285377a9efc4d7b26fff0ba0c339bb86
LeonidGHA <104501564+LeonidGHA@users.noreply.github.com>
2022-08-29 11:08:19 +0000

    Deploying to gh-pages from @ LeonidGHA/goit-react-hw-05-movies@ac067add900663f58f8af5b4ac8d26f2ab49ce2f 🚀

---
+++ static/js/721.5b285bd9.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[721],{4721:function(e,t,r){r.r(t);var a=r(184);t.default=function(){return(0,a.jsx)("h2",{children:"Not found page ... "})}}}]);
+//# sourceMappingURL=721.5b285bd9.chunk.js.map
