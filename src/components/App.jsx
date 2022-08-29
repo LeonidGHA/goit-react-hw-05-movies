ac067add900663f58f8af5b4ac8d26f2ab49ce2f
LeonidGHA <stowk1992@gmail.com>
2022-08-29 14:07:18 +0300

    add fix gh

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,32 +1,57 @@
-import { Route, Routes } from 'react-router-dom';
+import { Route, Routes, NavLink } from 'react-router-dom';
 import { lazy, Suspense } from 'react';
+import css from './App.module.css';
 
 // import Home from './page/Home/Home';
 // import Movies from './page/Movies/Movies';
 // import MoviesDetails from './page/Movies/MoviesDetails';
 // import Cast from './page/Movies/Cast/Cast';
 // import Reviews from './page/Movies/Reviews/Reviews';
-import SharedLayout from './SharedLayout';
 
 const Home = lazy(() => import('./page/Home/Home'));
 const Movies = lazy(() => import('./page/Movies/Movies'));
 const MoviesDetails = lazy(() => import('./page/Movies/MoviesDetails'));
 const Cast = lazy(() => import('./page/Movies/Cast/Cast'));
 const Reviews = lazy(() => import('./page/Movies/Reviews/Reviews'));
+const NotFound = lazy(() => import('./page/NotFound'));
 
 export const App = () => {
   return (
     <Suspense fallback={<div>Загрузка...</div>}>
-      <Routes>
-        <Route path="/" element={<SharedLayout />}>
-          <Route index element={<Home />} />
-          <Route path="Movies" element={<Movies />} />
-          <Route path="Movies/:id" element={<MoviesDetails />}>
+      <div className={css.container}>
+        <nav className={css.navigation}>
+          <NavLink
+            className={({ isActive }) =>
+              isActive
+                ? `${css.navigationLink} ${css.active}`
+                : `${css.navigationLink}`
+            }
+            to="/"
+          >
+            Home
+          </NavLink>
+          <NavLink
+            className={({ isActive }) =>
+              isActive
+                ? `${css.navigationLink} ${css.active}`
+                : `${css.navigationLink}`
+            }
+            to="/Movies"
+          >
+            Movies
+          </NavLink>
+        </nav>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/Movies" element={<Movies />} />
+          <Route path="/Movies/:id" element={<MoviesDetails />}>
             <Route path="Cast" element={<Cast />} />
             <Route path="Reviews" element={<Reviews />} />
           </Route>
-        </Route>
-      </Routes>
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </div>
+      ;
     </Suspense>
   );
 };
---
+++ src/components/page/NotFound.jsx
@@ -0,0 +1,5 @@
+const NotFound = () => {
+  return <h2>Not found page ... </h2>;
+};
+
+export default NotFound;
